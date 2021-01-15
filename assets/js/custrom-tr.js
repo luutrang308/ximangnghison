a0ed01e3f9e7ed8357cec3bd9692d0fa8ed135ee
luutrang308 <luutrangk10@gmail.com>
2021-01-15 09:54:17 +0700

    ximangnghison Trang

--- assets/js/custrom-tr.js
+++ assets/js/custrom-tr.js
@@ -15,6 +15,7 @@ $slickElement.slick({
   infinite: false,
   slidesToShow: 3,
   autoplay: false,
+  slidesToScroll: 1,
   dots: true,
   prevArrow: "<button class='prev slick-prev'><img class='left-arrow ' src='./assets/img/next3.png' alt=''></button>",
   nextArrow: "<button class='next slick-next'><img class='right-arrow ' src='./assets/img/next4.png' alt=''></button>",
@@ -264,3 +265,45 @@ $(document).ready(function() {
     
 });
 
+$.Tween.propHooks.number = {
+  get: function(tween) {
+    var num = tween.elem.innerHTML.replace(/^[^\d-]+/, "");
+    return parseFloat(num) || 0;
+  },
+
+  set: function(tween) {
+    var opts = tween.options;
+    tween.elem.innerHTML =
+      (opts.prefix || "") +
+      tween.now.toFixed(opts.fixed || 0) +
+      (opts.postfix || "");
+  }
+};
+
+$("#num-1").animate(
+  { number: 20 },
+  {
+    duration: 2000,
+    postfix: ""
+  }
+);
+
+$("#num-2")
+.delay(0)
+.animate(
+  { number: 100 },
+  {
+    duration: 1500,
+    postfix: "+"
+  }
+);
+
+$("#num-3")
+.delay(0)
+  .animate(
+  { number: 100 },
+  {
+      duration: 2000,
+      postfix: "+"
+  }
+);
